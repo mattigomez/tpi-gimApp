cf8ab652de347c9e790ddc138a15281666e87156
Juan Pablo Fernandez <149335938+JuanPabloFernandezAlvarez@users.noreply.github.com>
2025-05-09 20:47:18 -0300

    add Navbar with nav buttons and logo

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,55 @@
+
+import { Button} from "react-bootstrap";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+
+export const Header = (onLogout) => {
+  const handleNavigateAddRoutine = () => {
+    navigate("/home/add-routine", { replace: true });
+  };
+
+  const handleNavigateHome = () => {
+    navigate("/home", { replace: true });
+  };
+  return (
+    <Navbar fixed="top" expand="lg" bg="dark" data-bs-theme="dark">
+    <Container>
+      <Navbar.Brand href="#home">
+        <img
+          src="/src/assets/logowhite-GYMHUB.png"
+          alt="GYMHUB Logo"
+          height="60"
+          className="d-inline-block align-top"
+        />{" "}
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbar-content" />
+      <Navbar.Collapse id="navbar-content">
+        <Nav className="ms-auto">
+          {" "}
+          {}
+          <Button
+            variant="outline-light"
+            className="me-2"
+            onClick={handleNavigateHome}
+          >
+            Home
+          </Button>
+          <Button
+            variant="success"
+            className="me-2"
+            onClick={handleNavigateAddRoutine}
+          >
+            Agregar Rutina
+          </Button>
+          <Button variant="danger" onClick={onLogout}>
+            Cerrar sesión
+          </Button>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+  )
+}
+
+export default Header;
